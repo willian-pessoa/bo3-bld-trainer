edcb52a2814b1631f1a6bc06de090e8cd95197f1
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-11-10 18:22:06 -0300

    finish the timer UI

--- client/src/components/timer/session-stats.component.jsx
+++ client/src/components/timer/session-stats.component.jsx
@@ -1,23 +1,112 @@
-import React from 'react'
+import React from "react";
 
-import "./session-stats.styles.scss"
+import Table from "react-bootstrap/Table";
 
-const SessionStats = ({title}) => {
+import "./session-stats.styles.scss";
+
+const SessionStats = ({ title }) => {
   return (
-    <div className='session-stats'>
+    <div className="session-stats">
       <p>{title}</p>
       <div className="session-highlights">
-        <p>Single</p>
-        <p>Mo3</p>
-        <p>MBo3</p>
-        <p>Acurancy</p>
+        <Table bordered size="sm">
+          <thead>
+            <tr>
+              <th>Category</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Single</td>
+              <td>00:00</td>
+            </tr>
+            <tr>
+              <td>Mo3</td>
+              <td>00:00</td>
+            </tr>
+            <tr>
+              <td>MBo3</td>
+              <td>00:00</td>
+            </tr>
+            <tr>
+              <td>Accurancy</td>
+              <td>70%</td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
+      <p>Solves x / y</p>
       <div className="session-times">
-        <p>Solve x/y</p>
-        table
+        <Table bordered size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Time</th>
+              <th>Memo</th>
+              <th>Exec</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+            <tr>
+              <th>2</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+            <tr>
+              <th>3</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+            <tr>
+              <th>4</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+            <tr>
+              <th>5</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+            <tr>
+              <th>6</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+            <tr>
+              <th>7</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+            <tr>
+              <th>8</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+            <tr>
+              <th>9</th>
+              <th>00:00</th>
+              <th>00:00</th>
+              <th>00:00</th>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SessionStats
+export default SessionStats;
--- client/src/pages/timer/timerPage.jsx
+++ client/src/pages/timer/timerPage.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
 import Timer from "../../components/timer/timer.component";
+import BackButton from "../../components/timer/back-button.component";
+
+import { useSelector } from "react-redux";
+
+const style = {
+  backgroundColor: "#f0f4c3",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100vw",
+  height: "100vh",
+  flexDirection: "column"
+};
 
 const TimerPage = () => {
-  return <Timer />;
+  const isLogged = useSelector((state) => state.user.logged);
+  return isLogged ? (
+    <Timer />
+  ) : (
+    <div style={style}>
+      Need to Login <BackButton />
+    </div>
+  );
 };
 
 export default TimerPage;
